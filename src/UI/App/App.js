69ab32562977c70f701346e24901b82c362a7d10
Zoltan <zoltan@colourbox.com>
2018-12-07 14:48:29 +0100

    Added private/public route - about page

--- src/UI/App/App.js
+++ src/UI/App/App.js
@@ -1,24 +1,51 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import AsyncComponent from '../Components/AsyncComponent/AsyncComponent';
+// import PrivateRoute from '../Components/AsyncComponent/AsyncComponent';
+
+import SessionUser from '../../Globals/SessionUser';
 import './App.scss';
 
 const Login = AsyncComponent(() => import("../Containers/Login/Login"));
 const Folder = AsyncComponent(() => import("../Containers/Folder/Folder"));
 const NotFound = AsyncComponent(() => import("../Containers/NotFound/NotFound"));
+const About = AsyncComponent(() => import("../Containers/About/About"));
 
 class App extends React.Component {
   render() {
     return (
       <Router>
           <Switch>
-            <Route path="/" exact component={Login} />
-            <Route path="/folder/:id" exact component={Folder} />
+            <PublicRoute path="/" exact component={Login} />
+            <PrivateRoute path="/folder/:id" exact component={Folder} />
+            <Route path="/about" exact component={About} />
             <Route component={NotFound} />
           </Switch>
       </Router>
     );
   }
 }
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={(props) => (
+      SessionUser.isLogged === true
+      ? <Component {...props} />
+      : <Redirect to={{
+          pathname: "/",
+          state: { from: props.location }
+          }} />
+  )} />
+)
+
+const PublicRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={(props) => (
+      SessionUser.isLogged !== true
+      ? <Component {...props} />
+      : <Redirect to={{
+          pathname: "/",
+          state: { from: props.location }
+          }} />
+  )} />
+)
 
 export default App;
