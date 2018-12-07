ba6b58f560e03a74570a55aa15a54e28fccf3cf6
Zoltan <zoltan@colourbox.com>
2018-12-07 14:18:30 +0100

    Added event to session user and get folder

--- src/Globals/API.js
+++ src/Globals/API.js
@@ -1,25 +1,39 @@
 class API {
     constructor() {
         this.data = {
-            key: '',
+            token: '',
             endpoint: 'https://test-api.cbx.xyz',
         };
     }
 
-    get key() {
-        return this.data.key;
+    get token() {
+        return this.data.token;
     }
 
     get endpoint() {
         return this.data.endpoint;
     }
 
+    isValidToken(token) {
+        return /^[a-zA-Z0-9]{40}$/.test(token);
+    }
+
+    setToken(token) {
+        if (this.isValidToken(token)) {
+            this.data.token = token;
+            return token;
+        }
+        return false;
+    }
+
     fetch(path, options) {
         options = Object.assign({
             cache: 'no-cache',
             redirect: 'follow',
+            headers: {
+                Authorization: 'CBX-SIMPLE-TOKEN Token=' + this.token,
+            },
         }, options);
-        console.log(this.endpoint + path);
         return fetch(this.endpoint + path, options);
     }
 }
--- src/Globals/SessionUser.js
+++ src/Globals/SessionUser.js
@@ -1,12 +1,16 @@
+import EventTrigger from 'event-trigger';
 import API from './API';
 
 class SessionUser {
     constructor() {
+        this.events = new EventTrigger();
         this.data = {
             id: 0,
-            name: 'Peter Smith',
+            name: '',
         };
     }
+
+    /** Get */
 
     get name() {
         return this.data.name;
@@ -17,11 +21,25 @@ class SessionUser {
     }
 
     get isLogged() {
-        return this.userId;
+        return this.userId > 0;
     }
 
+    /** Set */
+
+    /** Event */
+
+    on(...args) {
+        this.events.on.apply(this.events, args);
+    }
+
+    off(...args) {
+        this.events.off.apply(this.events, args);
+    }
+
+    /** Other */
+
     checkUserCredential(apiToken) {
-        return new Promise(function(resolve, reject) {
+        return new Promise((resolve, reject) => {
             if (typeof apiToken !== 'string' || !(/^[a-zA-Z0-9]{40}$/.test(apiToken)) ) {
                 const error = new Error("Invalid token format")
                 error.code = 404;
@@ -29,6 +47,14 @@ class SessionUser {
             }
             API.fetch('/authenticate/simpletoken/' + apiToken).then(response => response.json()).then(response => {
                 if (response.TokenStatus === 'valid') {
+
+                    // doto: remove
+                    this.data.id = 100;
+                    this.data.name = 'Peter Smith';
+
+                    API.setToken(apiToken);
+                    this.events.trigger('change', this);
+
                     resolve(true);
                 } else {
                     const error = new Error("Invalid token status");
@@ -42,10 +68,39 @@ class SessionUser {
             });
         });
     }
+
+    getFolders() {
+        return new Promise((resolve, reject) => {
+            if (this.isLogged) {
+                return API.fetch('/folder')
+                .then(response => response.json())
+                .then(response => {
+                    resolve(response);
+                }).catch(response => {
+                    reject(response);
+                });
+            }
+            const error = new Error("User is not logged");
+            error.code = 401;
+            reject(error);
+        });
+    }
 }
 
 if (typeof window.globalSessionUser === 'undefined') {
     window.globalSessionUser = new SessionUser();
 }
+
+/*
+window.globalSessionUser.on('change', (event, user) => {
+    console.log(user);
+});
+
+window.globalSessionUser.checkUserCredential('e1d23c94654bac61b898a74d45e46f73d20a181e').then(response => {
+    window.globalSessionUser.getFolders().then(response => {
+        console.log(response);
+    });
+});
+*/
 
 export default window.globalSessionUser;
